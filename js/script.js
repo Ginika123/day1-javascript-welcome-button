0c7b5fa42a160d50c9700944b50a9471cf3927a8
blessing <ginikanwoye2020@gmail.com>
2026-02-25 16:23:02 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// i want javascript to target my id in my html the welcomeBtn
+const btn = document.getElementById("welcomeBtn")
+
+// i want javascript to make that button  to function
+btn.addEventListener("click", function() {
+    alert("Welcome to my website");
+    console.log("User click the buttton");
+});
